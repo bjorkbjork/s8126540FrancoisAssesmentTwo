2c3ec44d8f8560592b9c82066ea86cb4aa8b63ea
bjorkbjork <40650498+bjorkbjork@users.noreply.github.com>
2024-10-14 17:46:04 +1100

    Add files via upload

---
+++ app/src/main/java/com/example/s8126540francoisassessmenttwo/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.s8126540francoisassessmenttwo.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
